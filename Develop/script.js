cf2c65b6abb789d0758a563f2af84536b7673d70
Chris Rice <chrisrice2442@yahoo.com>
2023-10-25 14:08:17 -0400

    added 2 lines at beginning of js to delay code runnning

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,10 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
+
+var localeSettings = {};
+dayjs.locale(localeSettings);
+//I think this delays execution of code until DOM loads
 $(function () {
 
   var currentTime = dayjs();
